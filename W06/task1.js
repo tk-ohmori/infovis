edc9da75bb48d53a82adebfddb9e5a8d019b33b6
__ tak <ohmori@stu.kobe-u.ac.jp>
2022-04-27 14:44:03 +0900

    w06 task1

--- W06/task1.js
+++ W06/task1.js
@@ -6,7 +6,7 @@ d3.csv("https://tk-ohmori.github.io/infovis/W04/task1.csv")
             parent: '#drawing_region',
             width: 256,
             height: 256,
-            margin: {top:10, right:10, bottom:20, left:10}
+            margin: {top:10, right:10, bottom:20, left:30, xaxis:10, yaxis:10}
         };
 
         const scatter_plot = new ScatterPlot( config, data );
@@ -23,7 +23,7 @@ class ScatterPlot {
             parent: config.parent,
             width: config.width || 256,
             height: config.height || 256,
-            margin: config.margin || {top:10, right:10, bottom:10, left:10}
+            margin: config.margin || {top:10, right:10, bottom:10, left:10, xaxis:10, yaxis:10}
         }
         this.data = data;
         this.init();
@@ -49,13 +49,17 @@ class ScatterPlot {
             .range( [0, self.inner_height] );
 
         self.xaxis = d3.axisBottom( self.xscale )
-            .ticks(6);
+            .ticks(5)
+            .tickSize(10)
+            .tickPadding(3);
 
         self.xaxis_group = self.chart.append('g')
             .attr('transform', `translate(0, ${self.inner_height})`);
 
         self.yaxis = d3.axisLeft( self.yscale )
-        .ticks(6);
+            .ticks(8)
+            .tickSize(10)
+            .tickPadding(2);
 
         self.yaxis_group = self.chart.append('g')
             .attr('transform', `translate(0, 0)`);
@@ -66,11 +70,11 @@ class ScatterPlot {
 
         const xmin = d3.min( self.data, d => d.x );
         const xmax = d3.max( self.data, d => d.x );
-        self.xscale.domain( [xmin, xmax] );
+        self.xscale.domain( [xmin - self.config.margin.xaxis, xmax + self.config.margin.xaxis] );
 
         const ymin = d3.min( self.data, d => d.y );
         const ymax = d3.max( self.data, d => d.y );
-        self.yscale.domain( [ymin, ymax] );
+        self.yscale.domain( [ymin - self.config.margin.yaxis, ymax + self.config.margin.yaxis] );
 
         self.render();
     }
@@ -84,7 +88,7 @@ class ScatterPlot {
             .append("circle")
             .attr("cx", d => self.xscale( d.x ) )
             .attr("cy", d => self.yscale( d.y ) )
-            .attr("r", 10)
+            .attr("r", 5)
             //.attr("r", d => d.r );
 
         self.xaxis_group
