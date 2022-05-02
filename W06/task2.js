7b5b21a61979008e7287c98c1837085f5fa3d655
__ tak <ohmori@stu.kobe-u.ac.jp>
2022-05-02 13:22:09 +0900

    w06

--- W06/task2.js
+++ W06/task2.js
@@ -7,7 +7,8 @@ d3.csv("https://tk-ohmori.github.io/infovis/W04/task1.csv")
             width: 256,
             height: 256,
             margin: {top:10, right:10, bottom:20, left:30, xaxis:10, yaxis:10, label:20},
-            label: {x:'X-label', y:'Y-label'}
+            label: {x:'X-label', y:'Y-label'},
+            title: "Chart Title"
         };
 
         const scatter_plot = new ScatterPlot( config, data );
@@ -25,7 +26,8 @@ class ScatterPlot {
             width: config.width || 256,
             height: config.height || 256,
             margin: config.margin || {top:10, right:10, bottom:10, left:10, xaxis:10, yaxis:10},
-            label: config.label || {x:'xLabelName', y:'yLabelName'}
+            label: config.label || {x:'xLabelName', y:'yLabelName'},
+            title: config.title || "chartTitle"
         }
         this.data = data;
         this.init();
@@ -56,7 +58,7 @@ class ScatterPlot {
             .tickPadding(3);
 
         self.xaxis_group = self.chart.append('g')
-            .attr('transform', `translate(0, ${self.inner_height})`);
+            .attr('transform', `translate(${self.config.margin.label}, ${self.inner_height + self.config.margin.label})`);
 
         self.yaxis = d3.axisLeft( self.yscale )
             .ticks(8)
@@ -64,7 +66,7 @@ class ScatterPlot {
             .tickPadding(2);
 
         self.yaxis_group = self.chart.append('g')
-            .attr('transform', `translate(${self.config.margin.label}, 0)`);
+            .attr('transform', `translate(${self.config.margin.label}, ${self.config.margin.label})`);
     }
 
     update() {
@@ -72,11 +74,13 @@ class ScatterPlot {
 
         const xmin = d3.min( self.data, d => d.x );
         const xmax = d3.max( self.data, d => d.x );
-        self.xscale.domain( [xmin - self.config.margin.xaxis, xmax + self.config.margin.xaxis] );
+        const dx = self.config.margin.label + self.config.margin.left;
+        self.xscale.domain( [xmin-10, xmax+10] );
 
         const ymin = d3.min( self.data, d => d.y );
         const ymax = d3.max( self.data, d => d.y );
-        self.yscale.domain( [ymax + self.config.margin.yaxis, ymin - self.config.margin.yaxis] );
+        const dy = self.config.margin.label + self.config.margin.top;
+        self.yscale.domain( [ymax+10, ymin-10] );
 
         self.render();
     }
@@ -88,8 +92,8 @@ class ScatterPlot {
             .data(self.data)
             .enter()
             .append("circle")
-            .attr("cx", d => self.xscale( d.x ) )
-            .attr("cy", d => self.yscale( d.y ) )
+            .attr("cx", d => self.xscale( d.x ) + self.config.margin.label )
+            .attr("cy", d => self.yscale( d.y ) + self.config.margin.label )
             .attr("r", 5)
             //.attr("r", d => d.r );
 
@@ -100,17 +104,25 @@ class ScatterPlot {
             .call( self.yaxis );
 
         self.svg.append('text')
-            //.attr('text-anchor', 'mm')
-            .attr('x', parseInt(self.inner_width/2))
-            .attr('y', self.config.margin.label + self.config.margin.top + self.inner_height + parseInt(self.config.margin.label/2))
+            .attr('text-anchor', 'middle')
+            .attr('x',self.config.margin.label + self.config.margin.left + parseInt(self.inner_width/2))
+            .attr('y', self.config.margin.label + self.config.margin.top + self.inner_height + self.config.margin.label + self.config.margin.bottom)
             .text(self.config.label.x)
 
         self.svg.append('text')
-            .attr('text-anchor', 'mm')
-            .attr('x', parseInt(self.config.margin.label/2))
-            .attr('y', parseInt(self.inner_height/2))
-            // .attr('dy', )
+            .attr('text-anchor', 'middle')
             .attr('transform', 'rotate(-90)')
+            .attr('x', -(self.config.margin.label + self.config.margin.top + parseInt(self.inner_height/2)))
+            .attr('y', self.config.margin.label)
+            //.attr('dx', -15)
             .text(self.config.label.y)
+        
+        self.svg.append('text')
+            .attr('text-anchor', 'middle')
+            .attr('x',self.config.margin.label + self.config.margin.left + parseInt(self.inner_width/2))
+            .attr('y', self.config.margin.label)
+            .attr('font-size', 'large')
+            .attr('font-weight', 'bold')
+            .text(self.config.title)
     }
 }
