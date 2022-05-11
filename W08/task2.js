a0ff4e4e3a1e5b86aed161bfe19bf198cafa4f2e
tk-ohmori <ohmori@stu.kobe-u.ac.jp>
2022-05-11 22:21:15 +0900

    w08

--- W08/task2.js
+++ W08/task2.js
@@ -4,7 +4,7 @@ d3.csv("https://tk-ohmori.github.io/infovis/W08/data2.csv")
 
         var config = {
             parent: '#drawing_region',
-            width: 256,
+            width: 512,
             height: 256,
             margin: {top:30, right:10, bottom:30, left:30},
             title: 'タイトル'
@@ -36,61 +36,27 @@ class LineChart{
         self.svg = d3.select(self.config.parent)
             .attr('width', self.config.width)
             .attr('height', self.config.height);
-        
-// 以下修正
-        self.chart = self.svg.append('g')
-            .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`)
-        
-        self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
-        self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
-
-        self.xscale = d3.scaleLinear()
-            .range([0, self.inner_width]);
-        
-        self.yscale = d3.scaleBand()
-            .range([0, self.inner_height])
-            .paddingInner(0.1);
-        
-        self.xaxis = d3.axisBottom(self.xscale)
-            .ticks(6)
-            .tickSizeOuter(0);
-        
-        self.yaxis = d3.axisLeft(self.yscale)
-            .tickSizeOuter(0);
-
-        self.xaxis_group = self.chart.append('g')
-            .attr('transform', `translate(0, ${self.inner_height})`);
-        
-        self.yaxis_group = self.chart.append('g');
-
-        self.cahr_title = self.svg.append('text')
-            .attr('text-anchor', 'middle')
-            .attr('transform', `translate(256, 15)`)
-            .text(self.config.title);
+            
+        self.line = d3.line()
     }
 
     update(){
         let self = this;
 
-        self.xscale.domain([0, d3.max(self.data, d => d.value)]);
-        self.yscale.domain(self.data.map(d => d.label));
-
+        self.line
+            .x(d => d.x)
+            .y(d => d.y)
+        
         self.render();
     }
 
     render(){
         let self = this;
 
-        self.chart.selectAll("rect")
-            .data(self.data)
-            .enter()
-            .append("rect")
-            .attr("x", 0)
-            .attr("y", d => self.yscale(d.label))
-            .attr("width", d => self.xscale(d.value))
-            .attr("height", self.yscale.bandwidth());
-        
-        self.xaxis_group.call(self.xaxis);
-        self.yaxis_group.call(self.yaxis);
+        self.svg
+            .append('path')
+            .attr('d', line(data))
+            .attr('stroke', 'black')
+            .attr('fill', 'none');
     }
 }
