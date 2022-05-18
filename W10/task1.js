f889e3a5eea7b822552034c18b7c9fcfcf8a45c3
__ tak <ohmori@stu.kobe-u.ac.jp>
2022-05-18 13:57:01 +0900

    w10

---
+++ W10/task1.js
@@ -0,0 +1,112 @@
+d3.csv("https://tk-ohmori.github.io/infovis/W10/task1.csv")
+    .then( data => {
+        data.forEach( d => { d.value = +d.value;});
+        
+        var config = {
+            parent: '#drawing_region',
+            width: 512,
+            height: 256,
+            margin: {top:30, right:10, bottom:50, left:60},
+            title: '神戸市の人口',
+            label: {xaxis:'人数', yaxis:'区'}
+        };
+
+        const animmated_bar_chart = new AnimatedBarChart( config, data );
+        bar_chart.update();
+    })
+    .catch( error => {
+        console.log( error );
+    });
+
+class AnimatedBarChart{
+    constructor(config, data){
+        this.config = {
+            parent: config.parent,
+            width: config.width || 256,
+            height: config.height || 256,
+            margin: config.margin || {top:10, right:10, bottom:50, left:60},
+            title: config.title || 'Chart Title',
+            label: config.label || {xaxis:'横軸', yaxis:'縦軸'}
+        }
+        this.data = data;
+        this.init();
+    }
+
+    init(){
+        let self = this;
+
+        self.svg = d3.select(self.config.parent)
+            .attr('width', self.config.width)
+            .attr('height', self.config.height);
+
+        self.chart = self.svg.append('g')
+            .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);
+        
+        self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
+        self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
+
+        self.xscale = d3.scaleLinear()
+            .range([0, self.inner_width]);
+        
+        self.yscale = d3.scaleBand()
+            .range([0, self.inner_height])
+            .paddingInner(0.1);
+        
+        self.xaxis = d3.axisBottom(self.xscale)
+            .ticks(6)
+            .tickSizeOuter(0);
+        
+        self.yaxis = d3.axisLeft(self.yscale)
+            .tickSizeOuter(0);
+
+        self.xaxis_group = self.chart.append('g')
+            .attr('transform', `translate(0, ${self.inner_height})`);
+        
+        self.yaxis_group = self.chart.append('g');
+
+        self.chart_title = self.svg.append('text')
+            .attr('text-anchor', 'middle')
+            .attr('transform', `translate(${self.config.width/2}, ${self.config.margin.top/2})`)
+            .attr('font-weight', 'bold');
+        
+        self.xaxis_label = self.svg.append('text')
+            .attr('text-anchor', 'middle')
+            .attr('transform', `translate(${self.config.width/2}, ${self.config.height - 10})`)
+            .attr('font-weight', 'bold');
+        
+        self.yaxis_label = self.svg.append('text')
+            .attr('text-anchor', 'middle')
+            .attr('transform', `translate(7, ${self.config.height/2})`)
+            .attr('font-weight', 'bold');
+    }
+
+    update(){
+        let self = this;
+
+        self.xscale.domain([0, d3.max(self.data, d => d.value)]);
+        self.yscale.domain(self.data.map(d => d.label));
+
+        self.render();
+    }
+
+    render(){
+        let self = this;
+
+        self.chart.selectAll('rect')
+            .data(self.data)
+            .enter()
+            .append('rect')
+            .attr('x', 0)
+            .attr('y', d => self.yscale(d.label))
+            .attr('width', d => self.xscale(d.value))
+            .attr('height', self.yscale.bandwidth())
+        
+        self.xaxis_group.call(self.xaxis);
+        self.yaxis_group.call(self.yaxis);
+
+        self.chart_title.text(self.config.title);
+
+        self.xaxis_label.text(self.config.label.xaxis);
+        self.yaxis_label.text(self.config.label.yaxis);
+    }
+}
